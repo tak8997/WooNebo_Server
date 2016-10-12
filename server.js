f97f3ac448eb38acf2c9155f9beab178f8e953cd
Lim Pureum <dlavnfma22@naver.com>
2016-10-12 11:21:42 +0900

    Add image url information

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require('express');
-var mongo = require('mongodb').MongoClient;
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send(req.query);
+    let obj1 = {
+        item: 'item',
+        image: 'http://bookthumb.phinf.naver.net/cover/101/209/10120935.jpg?type=m140&udate=20160612',
+        url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=10961916',
+        price: 3000
+    }
+    let obj2 = {
+        item: 'item2',
+        image: 'http://bookthumb.phinf.naver.net/cover/101/614/10161496.jpg?type=m140&udate=20160218',
+        url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=9586856',
+        price: 10000
+    }
+    let obj3 = {
+        item: 'item3',
+        image: 'http://bookthumb.phinf.naver.net/cover/106/529/10652939.jpg?type=m140&udate=20160817',
+        url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=10362971',
+        price: 12300
+    }
+    let array = [obj1, obj2, obj3];
+
+    res.send(array);
 });
 
 app.listen(3000, function() {
