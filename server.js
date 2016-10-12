321607a288ed67f9fa9a03aa343c3bfd3b262c59
Lim Pureum <dlavnfma22@naver.com>
2016-10-12 11:26:57 +0900

    change variable type

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    let obj1 = {
+    var obj1 = {
         item: 'item',
         image: 'http://bookthumb.phinf.naver.net/cover/101/209/10120935.jpg?type=m140&udate=20160612',
         url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=10961916',
         price: 3000
     }
-    let obj2 = {
+    var obj2 = {
         item: 'item2',
         image: 'http://bookthumb.phinf.naver.net/cover/101/614/10161496.jpg?type=m140&udate=20160218',
         url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=9586856',
         price: 10000
     }
-    let obj3 = {
+    var obj3 = {
         item: 'item3',
         image: 'http://bookthumb.phinf.naver.net/cover/106/529/10652939.jpg?type=m140&udate=20160817',
         url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=10362971',
         price: 12300
     }
-    let array = [obj1, obj2, obj3];
+    var array = [obj1, obj2, obj3];
 
     res.send(array);
 });
