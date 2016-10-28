5c771b50729b8ef1763dcc360555feab4fdf1dce
Lim Pureum <dlavnfma22@naver.com>
2016-10-28 15:44:44 +0900

    create kiosk router module

--- server.js
+++ server.js
@@ -1,29 +1,8 @@
 var express = require('express');
 var app = express();
+var router = require('./router');
 
-app.get('/', function(req, res) {
-    var obj1 = {
-        item: 'item',
-        image: 'http://bookthumb.phinf.naver.net/cover/101/209/10120935.jpg?type=m140&udate=20160612',
-        url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=10961916',
-        price: 3000
-    }
-    var obj2 = {
-        item: 'item2',
-        image: 'http://bookthumb.phinf.naver.net/cover/101/614/10161496.jpg?type=m140&udate=20160218',
-        url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=9586856',
-        price: 10000
-    }
-    var obj3 = {
-        item: 'item3',
-        image: 'http://bookthumb.phinf.naver.net/cover/106/529/10652939.jpg?type=m140&udate=20160817',
-        url: 'http://book.naver.com/bookdb/book_detail.nhn?bid=10362971',
-        price: 12300
-    }
-    var array = [obj1, obj2, obj3];
-
-    res.send(array);
-});
+app.use(router);
 
 app.listen(3000, function() {
     console.log('Server started, localhost:3000');
